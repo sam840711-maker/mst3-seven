d2ea96c88e9ecd578f92821afc8ffd3ea56cc624
sam840711-maker <sam840711@gmail.com>
2026-09-13 17:02:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shtrend-v3';
+const CACHE='shtrend-v5';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-apple-180.png'];
 self.addEventListener('install',e=>{self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
